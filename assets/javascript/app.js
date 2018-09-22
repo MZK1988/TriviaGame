36d7f1d31ecafceeedd6f3bc744ab0f4df8384e9
MZK1988 <max@korenvaes.com>
2018-09-22 15:51:34 -0500

    another

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,63 @@
 //when start button clicks, initiate timer
 
 
-window.onload = stop();
-$("#startButton").on("click", run)
 
-var number = 120;
-var intervalID;
-console.log(number);
-function run() {
-    clearInterval(intervalID);
-    intervalID = setInterval(decrement, 1000);
-}
+// run($("#doneButton").on("click", function() {
+//     //creating a variable that stores the correct answer for each question 
+//      answerOne === "93 million";
+//      answerTwo === "8 planets";
+//      answerThree === "100 billion";
+//      answerFour === "Austin";
+//      answerFive === "Paris";
 
-function decrement() {
-    number--;
-    $("#timer").html("<strong>" +"Time Left: " + number + "</strong>")
-    if (number === 0) {
-        stop();
-        alert("Your time is up");
-    }
-}
+//      console.log("hello");
 
-function stop() {
-    clearInterval(intervalID);
-}
 
-run();
+//  }));
+
+//use one index.html web page instead of the landing page
+//must create three rows for the show and hide functions
+//  1. First row contains the div with the start button
+//  2. Second row contains the trivia questions
+//  3. Third row contains the review page when the timer runs out
+//when document ready function, hide the row divs containing the Trivia questions AND review page
+//on click start function, hide the row div containing the start button, show the row div containing the Trivia questions complete with timer, and hide the review page
+//if number ===0 on timer, replace the alert function with a hide function that hides the row divs with start button and Trivia questions, and shows the review page 
+
+$(document).ready(function () {
+    $("#hideTwo").hide();
+    $("#startButton").on("click", function () {
+        $("#hideOne").hide();
+        $("#hideTwo").show();
+        var number = 120;
+        var intervalID;
+        console.log(number);
+        function run() {
+            clearInterval(intervalID);
+            intervalID = setInterval(decrement, 1000);
+        }
+
+        function decrement() {
+            number--;
+            $("#timer").html("<strong>" + "Time Left: " + number + "</strong>")
+            if (number === 0) {
+                stop();
+                alert("Your time is up");
+            }
+        }
+
+        function stop() {
+            clearInterval(intervalID);
+        }
+
+        run();
+
+        
+
+    })
+
+
+
+});
+
 
